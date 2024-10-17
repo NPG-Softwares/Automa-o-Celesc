8bbadbe142b35f9a9c706b245910b421aacb4399
Nycolas Galdino <nycolaspimentel12@gmail.com>
2024-10-17 10:19:50 -0300

    modified: New method to get logins, accounts and units

--- app/Objects/Obj_ApiSpring.py
+++ app/Objects/Obj_ApiSpring.py
@@ -3,10 +3,8 @@ import dotenv
 import requests as req
 from typing import Literal
 from functools import cache
-from datetime import datetime as dt
-from datetime import timedelta as td
 
-from Objects.Obj_ApiSpringBase import BaseClient, BaseInvoice
+from Objects.Obj_ApiSpringBase import BaseControleDownload, BaseInvoice, BaseUnit
 
 dotenv.load_dotenv()
 
@@ -20,6 +18,14 @@ class LoginError(Exception):
     pass
 
 
+class UploadError(Exception):
+    pass
+
+
+class API_Spring_Error(Exception):
+    pass
+
+
 # ------------------------------------------------- Classes
 class API_Spring:
     _token = {}
@@ -28,12 +34,12 @@ class API_Spring:
         self.ambient = ambient
         self.__get_envs__()
 
-        if API_Spring._token.get(self.ambient):
+        if API_Spring._token.get(self.ambient, None):
             self.token = API_Spring._token.get(self.ambient)
 
         else:
             self.token = API_Spring._token.get(self.ambient) or self.get_token()
-            API_Spring._token.setdefault(self.ambient, self.token)
+            API_Spring._token[self.ambient] = self.token
 
     def __get_envs__(self) -> None:
         if self.ambient == 'hml':
@@ -41,9 +47,12 @@ class API_Spring:
             self.password = os.getenv('hml_login_password')
             self.end_token = os.getenv('hml_end_get_token')
             self.end_logins = os.getenv('hml_end_logins')
-            self.end_get_invoices = os.getenv('hml_end_get_invoices')
             self.end_up_invoice = os.getenv('hml_end_up_invoice')
+            self.end_get_invoices = os.getenv('hml_end_get_invoices')
+            self.end_send_error_log = os.getenv('hml_end_send_error_log')
             self.end_get_fornecedores_id = os.getenv('hml_end_get_fornecedores_id')
+            self.end_get_unidades_by_id = os.getenv('hml_end_get_unidades_by_id')
+            self.end_get_account_type_id = os.getenv('hml_end_get_account_type_id')
 
         elif self.ambient == 'prod':
             self.email = os.getenv('prod_login_email')
@@ -52,14 +61,17 @@ class API_Spring:
             self.end_logins = os.getenv('prod_end_logins')
             self.end_up_invoice = os.getenv('prod_end_up_invoice')
             self.end_get_invoices = os.getenv('prod_end_get_invoices')
+            self.end_send_error_log = os.getenv('prod_end_send_error_log')
             self.end_get_fornecedores_id = os.getenv('prod_end_get_fornecedores_id')
+            self.end_get_unidades_by_id = os.getenv('prod_end_get_unidades_by_id')
+            self.end_get_account_type_id = os.getenv('prod_end_get_account_type_id')
 
         else:
             raise AmbientError('Ambiente inválido, utilize os ambientes '
                                '[hml | prod] sendo "hml" (homologação) ou '
                                '"prod" (produção).')
 
-    def get_logins(self, fornecedor_id: str = None) -> dict:
+    def get_controle_download(self, fornecedor_id: str = None, cliente_id: str = None) -> dict:
         url = self.end_logins
         headers = {}
         headers['Authorization'] = self.token
@@ -68,6 +80,9 @@ class API_Spring:
 
         if fornecedor_id:
             params['fornecedorId'] = fornecedor_id
+
+        if cliente_id:
+            params['clienteId'] = cliente_id
 
         r = req.get(url, headers=headers, params=params)
 
@@ -83,7 +98,47 @@ class API_Spring:
 
         r = req.get(url, headers=headers, params=params)
 
-        return [x['id'] for x in r.json()['data']]
+        return [(x['id'], x['clienteAdmId']) for x in r.json()['data']]
+
+    def get_unidade_by_id(self, id: int) -> list:
+        url = self.end_get_unidades_by_id
+        url = url.replace('{id}', str(id))
+        headers = {}
+        headers['Authorization'] = self.token
+
+        r = req.get(url, headers=headers)
+
+        return r.json()
+
+    def get_account_type_id(self, name: str, client_id: int = None) -> int:
+        url = self.end_get_account_type_id
+
+        headers = {}
+        headers['Authorization'] = self.token
+
+        params = {}
+        params['Nome'] = name
+
+        if client_id:
+            params['ClienteId'] = client_id
+
+        r = req.get(url, headers=headers, params=params)
+
+        if r.status_code == 200:
+            return r.json()['data'][0]['id']
+
+    def new_request(self, method, **kwargs) -> dict:
+        if not kwargs.get('headers'):
+            kwargs['headers'] = {}
+
+        kwargs['headers']['Authorization'] = self.token
+
+        r = req.request(method, **kwargs)
+
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise API_Spring_Error(f'Erro [{r.status_code}]: {r.text}')
 
     @cache
     def get_token(self) -> str:
@@ -110,23 +165,20 @@ class API_Spring:
         else:
             raise LoginError(f'Erro [{r.status_code}]: {r.text}')
 
-    def get_accounts(self, client: BaseClient) -> dict:
-        now = dt.now()
+    def get_accounts(self, client: BaseControleDownload) -> dict:
         url = self.end_get_invoices
         headers = {}
         headers['Authorization'] = self.token
+        headers['Status'] = 'PENDENTE'
 
         params = {}
-        params['clientId'] = client.cliente_id
-        params['fornecedorId'] = client.fornecedor_id
-        params['vencimentoMinimo'] = now.strftime('%Y-%m-%d')
-        params['vencimentoMaximo'] = (now + td(days=60)).strftime('%Y-%m-%d')
+        params['ClienteId'] = client.cliente_id
 
         r = req.get(url, headers=headers, params=params)
 
         return r.json()
 
-    def up_invoice(self, payload: dict, files: dict) -> None:
+    def up_invoice(self, payload: dict, files: list[tuple]) -> None:
         """
         Uploads an invoice to the API using the provided payload and files.
 
@@ -143,75 +195,134 @@ class API_Spring:
         headers = {}
         headers['Authorization'] = self.token
 
-        for file in files.values():
+        for file in files:
             f = {'files': file}
+            temp_payload = payload.copy()
 
-            r = req.post(url, headers=headers, data=payload, files=f)
+            if file[0] == temp_payload['ArquivoAzurePdf']:
+                temp_payload['ArquivoAzureDigital'] = None
+                temp_payload['NomeArquivoDigital'] = None
+            else:
+                temp_payload['ArquivoAzurePdf'] = None
+                temp_payload['NomeArquivoPdf'] = None
+
+            r = req.post(url, headers=headers, data=temp_payload, files=f)
 
             if r.status_code == 200:
-                print(f'\t\t\tArquivo {f.get("files")[0]} enviado com sucesso.')
+                print(f'Arquivo {f.get("files")[0]} enviado com sucesso.')
             elif r.status_code == 413:
                 raise req.exceptions.ContentDecodingError(f'{file[0]}: Arquivo muito grande para ser enviado.')
             elif r.status_code != 200:
                 raise req.exceptions.RequestException(f'Erro [{r.status_code}]: {r.text}')
-        print('\t\t\tFatura enviada com sucesso.')
+        print('Fatura enviada com sucesso.')
+
+    def send_error_log(self, title: str, message: str, stacktrace: str,
+                       origin: str, status: Literal['warning', 'error', 'info', 'success'] = 'error',
+                       login_id: int = 1, cliente_adm_id: int = None) -> None:
+        url = self.end_send_error_log
+        headers = {}
+        headers['Authorization'] = self.token
+
+        payload = {
+            'titulo': title,
+            'mensagem': message,
+            'infoCode': stacktrace,
+            'origem': origin,
+            'status': status,
+            'loginId': login_id,
+            'clienteAdmId': cliente_adm_id
+        }
+
+        r = req.post(url, headers=headers, json=payload)
+
+        if r.status_code == 200:
+            print('Log enviado com sucesso!')
+        else:
+            print('Payload:', payload)
+            raise UploadError(f'Erro [{r.status_code}]: {r.text}')
 
 
-def filter_logins(api: API_Spring, logins: dict) -> list[BaseClient]:
+def filter_controle_download(api: API_Spring, list_controles_download: dict,
+                             fornecedores_ids: list[int], ambient: Literal['prod', 'hml']) -> list[BaseControleDownload]:
     filtered_accounts: list[str] = []
-    filtered_logins: list[BaseClient] = []
+    filtered_controle_download: list[BaseControleDownload] = []
 
-    assert len(logins) > 0, 'Nenhum login encontrado.'
-    for login in logins:
+    assert len(list_controles_download) > 0, 'Nenhum login encontrado.'
+    for controle_download in list_controles_download:
 
-        if login['login'] == '' or login['login'] is None:
+        if controle_download['login'] == '' or controle_download['login'] is None:
             continue
 
-        if login['senha'] == '' or login['senha'] is None:
+        if controle_download['senha'] == '' or controle_download['senha'] is None:
             continue
 
-        if login['login'] in ['00000000000', '000.000.000.00']:
+        if controle_download['login'] in ['00000000000', '000.000.000.00']:
             continue
 
-        client = BaseClient(login)
-        accounts = api.get_accounts(client)['data']
+        obj_controle_download = BaseControleDownload(controle_download)
+        obj_controle_download.ambient = ambient
+
+        accounts = []
+        accounts = api.get_accounts(obj_controle_download)['data']
 
         for account in accounts:
             obj_account = BaseInvoice(account)
+
             if obj_account.numero_conta in filtered_accounts:
                 continue
 
-            client.contas.append(obj_account)
+            if obj_account.fornecedor_id not in fornecedores_ids:
+                continue
+
+            if obj_account.controle_download_id != obj_controle_download.id:
+                continue
+
+            obj_controle_download.contas.append(obj_account)
             filtered_accounts.append(obj_account.numero_conta)
 
-        filtered_logins.append(client)
+        # unidades = [x.unidade_id for x in obj_controle_download.contas]
+        # unidades = list(set(unidades))
 
-    return filtered_logins
+        # for unidade_id in unidades:
+        #     if not unidade_id:
+        #         raise API_Spring_Error('Unidade não encontrada.')
+
+        #     unidade = api.get_unidade_by_id(unidade_id)
+        #     obj_unidade = BaseUnit(unidade)
+        #     obj_controle_download.unidades.append(obj_unidade)
+
+        # obj_controle_download.unidades = list(set(obj_controle_download.unidades))
+        # obj_controle_download.cnpjs = list(set([x.cnpj for x in obj_controle_download.unidades]))
+
+        filtered_controle_download.append(obj_controle_download)
+
+    return filtered_controle_download
 
 
-def get_logins(ambient: Literal['prod', 'hml'] = 'prod') -> list[BaseClient]:
-    """
-    Retrieves all logins for the given ambient (environment) and filters out
-    invalid logins.
-
-    Args:
-        ambient (Literal['prod', 'hml'], optional): The ambient to retrieve
-            logins from. Defaults to 'prod'.
-
-    Returns:
-        list: A list of filtered logins.
-    """
-
+def get_logins(name: str, ambient: Literal['prod', 'hml'] = 'prod') -> list[BaseControleDownload]:
     api = API_Spring(ambient)
-    name = 'CELESC'
     fornecedores_ids = api.get_fornecedores_by_name(name)
-    logins = []
-    for fornecedor_id in fornecedores_ids:
-        logins += api.get_logins(fornecedor_id)['data']
+    list_fornecedores_ids = list(set([x for x, _ in fornecedores_ids]))
+    controle_download_list = []
 
-    logins = filter_logins(api, logins)
-    return logins
+    for fornecedor_id, cliente_id in fornecedores_ids:
+        all_controle_download = api.get_controle_download(fornecedor_id=fornecedor_id, cliente_id=cliente_id)['data']
+        controle_download_list.extend(all_controle_download)
+
+    controle_download = filter_controle_download(api, controle_download_list, list_fornecedores_ids, ambient=ambient)
+    return controle_download
 
 
-if __name__ == '__main__':
-    get_logins('hml')
+def send_log(ambient: Literal['prod', 'hml'], title: str, message: str, stacktrace: str,
+             origin: str, cliente_adm_id: int, status: Literal['warning', 'error', 'info', 'success'] = 'error',
+             login_id: int = 1):
+    api = API_Spring(ambient)
+    api.send_error_log(
+        title=title,
+        message=message,
+        stacktrace=stacktrace,
+        origin=origin,
+        status=status,
+        login_id=login_id,
+        cliente_adm_id=cliente_adm_id
+    )
--- app/Objects/Obj_ApiSpringBase.py
+++ app/Objects/Obj_ApiSpringBase.py
@@ -1,4 +1,7 @@
-class BaseClient:
+from typing import Literal
+
+
+class BaseControleDownload:
     def __init__(self, data: dict) -> None:
         self.id = data.get("id")
         self.status = data.get("status")
@@ -9,17 +12,20 @@ class BaseClient:
         self.cnpj_login = data.get("cnpjLogin")
         self.cpf_gestor = data.get("cpfGestor")
         self.email_gestor = data.get("emailGestor")
-        self.senha = data.get("senha")
-        self.login = data.get("login")
+        self.senha = data.get("senha").strip()
+        self.login = data.get("login").strip()
         self.fatura_enviada_por_email = data.get("faturaEnviadaPorEmail")
         self.email_recebimento_fatura = data.get("emailRecebimentoFatura")
         self.dia_verificacao = data.get("diaVerificacao")
         self.nome_configuracao = data.get("nomeConfiguracao")
         self.observacao = data.get("observacao")
+        self.ambient: Literal['prod', 'hml']
+        self.cnpjs: list[str] = []
         self.contas: list[BaseInvoice] = []  # Empty list for potential future use
+        self.unidades: list[BaseUnit] = []  # Empty list for potential future use
 
     def __repr__(self) -> str:
-        return f"BaseClient(id={self.id}, status={self.status})"
+        return f"BaseClient(ControleDownloadId={self.id}, ClienteId={self.cliente_id})"
 
 
 class BaseInvoice:
@@ -27,16 +33,73 @@ class BaseInvoice:
         self.status = data.get("status")
         self.numero_conta = data.get("numeroConta")
         self.tipo_conta_id = data.get("tipoContaId")
-        self.codigo_fornecedor = data.get("codigoFornecedor")
         self.fornecedor_classe_id = data.get("fornecedorClasseId")
         self.fornecedor_id = data.get("fornecedorId")
-        self.conta_id = data.get("contaId")
-        self.arquivo_azure_digital = data.get("arquivoAzureDigital")
-        self.vencimento = data.get("vencimento")
+        self.conta_id = data.get("id")
         self.cliente_id = data.get("clienteId")
+        self.controle_download_id = data.get("controleDownloadId")
         self.unidade_id = data.get("unidadeId")
-        self.id = data.get("id")
         self.login_id = data.get("loginId")
 
     def __repr__(self) -> str:
-        return f"BaseInvoice(id={self.numero_conta}, status={self.status})"
+        return f"BaseInvoice(conta={self.numero_conta}, contaId={self.conta_id}, status={self.status})"
+
+
+class BaseUnit:
+    def __init__(self, data: dict) -> None:
+        self.status = data.get("status")
+        self.active = data.get("active")
+        self.cliente_adm_id = data.get("clienteAdmId")
+        self.login_id = data.get("loginId")
+        self.cliente_id = data.get("clienteId")
+        self.nome_fantasia = data.get("nomeFantasia")
+        self.razao_social = data.get("razaoSocial")
+        self.cnpj = data.get("cnpj")
+        self.principal = data.get("principal")
+        self.logradouro = data.get("logradouro")
+        self.logradouro_numero = data.get("logradouroNumero")
+        self.logradouro_complemento = data.get("logradouroComplemento")
+        self.logradouro_cep = data.get("logradouroCep")
+        self.logradouro_bairro = data.get("logradouroBairro")
+        self.logradouro_municipio = data.get("logradouroMunicipio")
+        self.logradouro_uf = data.get("logradouroUf")
+        self.observacao = data.get("observacao")
+        self.contas_relacionadas = []
+
+
+class FaturaInfo:
+    def __init__(self):
+        self.ArquivoAzureDigital: str
+        self.ArquivoAzurePdf: str
+        self.AnoReferencia: str
+        self.CicloInicio: str
+        self.CicloFim: str
+        self.ClienteId: int
+        self.CodigoBarra: str
+        self.ContaId: int
+        self.DownloadArquivo: str
+        self.Emissao: str
+        self.FornecedorClasseId: int
+        self.FornecedorId: int
+        self.LoginId: int
+        self.MesReferencia: str
+        self.MedicaoString: str
+        self.NomeArquivoDigital: str
+        self.NomeArquivoPdf: str
+        self.NumeroConta: str
+        self.Observacao: str
+        self.Status: str
+        self.TipoContaId: int
+        self.UnidadeId: int
+        self.ValorDocumentoDigital: float
+        self.ValorDocumentoPDF: float
+        self.Vencimento: str
+
+    def __str__(self) -> str:
+        return str(self.__dict__)
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+    def get_dict(self) -> dict:
+        return self.__dict__
